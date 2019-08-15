b7be181de414c06b23a50ba14d1440c53103bd23
Dejan Mitrovic <dejan.mitrovic112@gmail.com>
2019-08-15 21:43:25 +0200

    deploy

---
+++ src/lang/en.js
@@ -0,0 +1,11 @@
+const lang = {
+  contact: 'Contact',
+  born: 'Born',
+  bornIn: 'in',
+  experience: 'Experience',
+  education: 'Education',
+  skills: 'Skills',
+  about: 'About me',
+  hobbies: 'Hobbies'
+};
+export default lang;
---
+++ src/lang/fr.js
@@ -0,0 +1,10 @@
+const lang = {
+  contact: 'Contact',
+  born: 'Né en',
+  bornIn: 'à',
+  experience: 'Expérience professionelle',
+  education: 'Formation',
+  skills: 'Compétences',
+  about: 'À propos de moi'
+};
+export default lang;
